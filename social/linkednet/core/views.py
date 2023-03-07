c171cb00ccc81b20cefd7a52ef946809f87ecd49
A Bilahari <abilahari2023a@mca.ajce.in>
2023-03-07 15:01:10 +0530

    Admin login fix

--- social/linkednet/core/views.py
+++ social/linkednet/core/views.py
@@ -284,7 +284,10 @@ def signin(request):
 
         if user is not None:
             auth.login(request, user)
-            return redirect('/')
+            if user.is_superuser:
+                return redirect('admin/')
+            else:
+                return redirect('signin')
         else:
             messages.info(request, 'Credentials Invalid')
             return redirect('signin')
